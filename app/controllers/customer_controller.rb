03065801929cc4086678ea537d9b3ea2354aeae9
DavidBeauchamp78 <davidcbeauchamp@hotmail.com>
2020-03-24 12:31:00 -0500

    final

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,2 +1,11 @@
 class CustomerController < ApplicationController
+    def index
+        @customers = Customer.all
+    end
+    def alphabetized
+        @customers = Customer.order(name: :desc)
+    end
+    def missing_email
+        @customers = Customer.where("email_address = ''")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  get 'customers/', to: 'customer#index', as: 'customer' 
+  get 'customers/alphabetized', to: 'customer#alphabetized', as: 'customer/alphabetized' 
+  get 'customers/missing_email', to: 'customer#missing_email', as: 'customer/missing_email'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
